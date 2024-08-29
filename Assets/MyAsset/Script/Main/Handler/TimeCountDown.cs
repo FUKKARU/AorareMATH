7c7e773968fbc7259c4839badfa8ff02d17dfcad
msyn-yamada <ya1359@icloud.com>
2024-08-29 21:59:47 +0900

    [change]GameStateの書き換え、メンバ関数をプロパティに

--- Assets/MyAsset/Script/Main/Handler/TimeCountDown.cs
+++ Assets/MyAsset/Script/Main/Handler/TimeCountDown.cs
@@ -6,22 +6,29 @@ namespace Main.Handler
     {
         [SerializeField] private TMPro.TextMeshProUGUI text;
 
-        private float t;
+        private float _t;
+
+        // プロパティ
+        internal float T { 
+            get { return _t; }
+            set { _t = Mathf.Clamp(value, byte.MinValue, byte.MaxValue); }
+        }
 
         private void Start()
         {
-            t = SO.SO_Handler.Entity.InitTimeLimt;
+            _t = SO.SO_Handler.Entity.InitTimeLimt;
         }
 
         private void Update()
         {
             if (GameManager.Instance.State == GameState.Stay)
             {
-                if (t > 0) t -= Time.deltaTime;
-                t = Mathf.Max(0, t);
+                if (_t > 0) _t -= Time.deltaTime;
+                _t = Mathf.Max(0, _t);
+                if (_t == 0) GameManager.Instance.State = GameState.Over;
             }
 
-            if (text) text.text = $"{(int)t:D2}";
+            if (text) text.text = $"{(int)_t:D2}";
         }
     }
 }
