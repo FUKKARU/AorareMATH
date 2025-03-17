b4be7e2aeaccd6853a2d3f53811ec38934fc446f
foriver4725 <hrt67@icloud.com>
2025-03-17 19:10:28 +0900

    残り時間が少ない時の演出

--- Assets/MyAsset/Script/Main/Handler/TimeShower.cs
+++ Assets/MyAsset/Script/Main/Handler/TimeShower.cs
@@ -2,15 +2,52 @@
 using UnityEngine.UI;
 using General.Extension;
 using SO;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using DG.Tweening;
 
 namespace Main.Handler
 {
     internal sealed class TimeShower : MonoBehaviour
     {
+        [SerializeField] private Volume volume;
         [SerializeField] private Image fillImage;
         [SerializeField] private Color normalColor;
         [SerializeField] private Color dangerColor;
-        [SerializeField, Range(0.01f, 0.99f)] private float dangerThreshold;
+        [SerializeField, Range(0.01f, 0.99f)] private float timerRedThresholdRatio;
+        [SerializeField, Range(0.01f, 30.0f)] private float screenRedThresholdTime;
+
+        private Vignette vignette;
+        private Tween vignetteTween;
+        private bool isVignetteActiveThisFrame = false;
+        private bool isVignetteActivePrevFrame = false;
+
+        private void OnEnable()
+        {
+            if (volume != null)
+            {
+                if (volume.profile.TryGet(out Vignette vignette))
+                {
+                    this.vignette = vignette;
+                    this.vignette.intensity.Override(0);
+                }
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (isVignetteActiveThisFrame && !isVignetteActivePrevFrame)
+                StartVignetteTween();
+            else if (!isVignetteActiveThisFrame && isVignetteActivePrevFrame)
+                StopVignetteTween();
+            isVignetteActivePrevFrame = isVignetteActiveThisFrame;
+            isVignetteActiveThisFrame = false;
+        }
+
+        private void OnDisable()
+        {
+            StopVignetteTween();
+        }
 
         internal void UpdateTimeUI(float remainTime)
         {
@@ -18,7 +55,33 @@ namespace Main.Handler
             {
                 float fillAmount = remainTime.Remap(SO_Handler.Entity.InitTimeLimt, 0, 1, 0);
                 fillImage.fillAmount = fillAmount;
-                fillImage.color = fillAmount < dangerThreshold ? dangerColor : normalColor;
+                fillImage.color = fillAmount < timerRedThresholdRatio ? dangerColor : normalColor;
+            }
+
+            isVignetteActiveThisFrame =
+                GameManager.Instance.State == GameState.OnGoing
+                && remainTime < screenRedThresholdTime;
+        }
+
+        private void StartVignetteTween()
+        {
+            StopVignetteTween();
+
+            vignetteTween = DOTween.To(
+                () => 0,
+                x => vignette.intensity.Override(x),
+                0.6f,
+                0.8f
+            ).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        }
+
+        private void StopVignetteTween()
+        {
+            if (vignetteTween != null)
+            {
+                vignetteTween.Kill();
+                vignetteTween = null;
+                vignette.intensity.Override(0);
             }
         }
     }
