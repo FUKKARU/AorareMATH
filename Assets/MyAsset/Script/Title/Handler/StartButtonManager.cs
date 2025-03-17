703a31a779d25f966a49e4e3889282e1738321dd
foriver4725 <hrt67@icloud.com>
2025-03-17 16:49:31 +0900

    ボタンの処理共通化、演出を改善

--- Assets/MyAsset/Script/Title/Handler/StartButtonManager.cs
+++ Assets/MyAsset/Script/Title/Handler/StartButtonManager.cs
@@ -1,47 +1,19 @@
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using General;
 using General.Extension;
 using SO;
 using Ct = System.Threading.CancellationToken;
-using Text = TMPro.TextMeshProUGUI;
 
 namespace Title.Handler
 {
-    internal sealed class StartButtonManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+    internal sealed class StartButtonManager : AButtonManager
     {
-        [SerializeField] private GameObject startButtonParent;
-        [SerializeField] private Text text;
-        [SerializeField] private Color normalColor;
-        [SerializeField] private Color hoverColor;
-
         [SerializeField] private Transform loadImageTf;
         [SerializeField] private BGMPlayer bgmPlayer;
-        [SerializeField] private AudioSource clickSeAuidoSource;
 
-        private bool hasClicked = false;
-
-        public void OnPointerEnter(PointerEventData eventData)
-        {
-            if (hasClicked) return;
-            text.color = hoverColor;
-        }
-
-        public void OnPointerExit(PointerEventData eventData)
-        {
-            if (hasClicked) return;
-            text.color = normalColor;
-        }
-
-        public void OnPointerClick(PointerEventData eventData)
-        {
-            if (hasClicked) return;
-            hasClicked = true;
-            clickSeAuidoSource.Raise(SO_Sound.Entity.ClickSE, SoundType.SE);
-            Load(destroyCancellationToken).Forget();
-        }
+        protected override void OnClickImpl() => Load(destroyCancellationToken).Forget();
 
         private async UniTaskVoid Load(Ct ct)
         {
