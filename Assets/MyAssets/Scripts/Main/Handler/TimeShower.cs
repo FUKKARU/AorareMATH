347c04f686d72c4442b3dc3e4bb023ed5cd909ef
foriver4725 <hrt67@icloud.com>
2025-03-28 22:17:41 +0900

    feature: 数値微調整、UI画像自体の改善が必要か？

--- Assets/MyAssets/Scripts/Main/Handler/TimeShower.cs
+++ Assets/MyAssets/Scripts/Main/Handler/TimeShower.cs
@@ -56,7 +56,17 @@ namespace Main.Handler
 
             if (timerImage != null)
             {
-                timerImage.fillAmount = fillAmount.Remap(0.0f, 1.0f, 0.5f, 1.0f);
+                // 手動で補正する
+                // 0.90-0.60 は少し増やし、
+                // 0.40-0.10 は少し減らす
+                // 少しずれているが、大体こんな感じの数値で良いと思う
+                float timerFillAmount = fillAmount;
+                if (timerFillAmount is (> 0.60f and < 0.90f))
+                    timerFillAmount += Mathf.Abs(timerFillAmount - 0.75f).Remap(0.0f, 0.15f, 0.01f, 0.0f);
+                else if (timerFillAmount is (> 0.10f and < 0.40f))
+                    timerFillAmount -= Mathf.Abs(timerFillAmount - 0.25f).Remap(0.0f, 0.15f, 0.016f, 0.0f);
+
+                timerImage.fillAmount = timerFillAmount.Remap(0.0f, 1.0f, 0.5f, 1.0f);
                 timerImage.color = fillAmount < timerRedThresholdRatio ? dangerColor : normalColor;
             }
 
@@ -67,8 +77,8 @@ namespace Main.Handler
                 needleTransform.localEulerAngles = rot;
 
                 float th = fillAmount.Remap(0.0f, 1.0f, Mathf.PI, 0.0f);
-                float x = 253 * Mathf.Cos(th) + 456;
-                float y = 140 * Mathf.Sin(th) - 505;
+                float x = 257.0f * Mathf.Cos(th) + 453.5f;
+                float y = 140.0f * Mathf.Sin(th) - 505.0f;
                 needleTransform.localPosition = new Vector3(x, y, 0.0f);
             }
 
