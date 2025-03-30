e1dacf708926b42a5f68a0c7308d15c635f14e5f
msyn-yamada <ya1359@icloud.com>
2025-03-30 22:50:08 +0900

    数式データの追加

---
+++ Assets/MyAssets/Editor/QuestionGeneratingPrograms/1d1-2d1-as1.py
@@ -0,0 +1,43 @@
+import itertools
+import random
+
+
+def solve(numbers: list, target: int) -> str:
+    OFFSET = 1e-6
+    OS = {"+", "-"}
+    for e in itertools.permutations(numbers):
+        for o in OS:
+            eqs = [
+                f"{e[0]} {o[0]} {e[1]}",
+            ]
+            for eq in eqs:
+                try:
+                    if abs(eval(eq) - target) < OFFSET:
+                        return eq
+                except:
+                    continue
+    return None
+
+
+NUM = 10000
+with open("1d1-2d1-as1.txt", "w", encoding="utf-8") as f:
+    num_100 = NUM // 100
+    i = 0
+    while True:
+        number_2d = random.randint(10, 99)
+        number_1d = random.randint(1, 9)
+        target = random.randint(5, 100)
+        ret = solve([number_2d, number_1d], target)
+        if not ret:
+            continue
+
+        tens = number_2d // 10
+        ones = number_2d %  10
+        f.write(
+            f"{ones}, {tens},{number_1d}, {target}, {ret.replace(' ', '')}\n"
+        )
+        i += 1
+        if i >= NUM:
+            break
+        elif (i - 1) % num_100 == 0:
+            print(f"{(i-1) // num_100} % done")
