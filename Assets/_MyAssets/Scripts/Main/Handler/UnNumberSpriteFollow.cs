94a9f242ffa2a48959e220d19bf89e5470310d8a
foriver4725 <hrt67@icloud.com>
2025-07-27 22:17:19 +0900

    括弧を端に持ってきた

--- Assets/_MyAssets/Scripts/Main/Handler/UnNumberSpriteFollow.cs
+++ Assets/_MyAssets/Scripts/Main/Handler/UnNumberSpriteFollow.cs
@@ -129,10 +129,9 @@ namespace Main.Handler
 
             // モバイルでは、指で隠れてしまうので、拡大する
             // SpriteFollow 系統クラスでの共通処理
+            // はめ込む時、このクラスのインスタンスは削除されるので、拡大しっぱなしでいいと思う
 #if UNITY_IOS || UNITY_ANDROID
-            thisInstance.transform.localScale = new(hoverScaleWhenMobile, hoverScaleWhenMobile, 1.0f);
-#else
-            thisInstance.transform.localScale = Vector3.one;
+            thisInstance.transform.SetScaleXY(hoverScaleWhenMobile, hoverScaleWhenMobile);
 #endif
         }
 
